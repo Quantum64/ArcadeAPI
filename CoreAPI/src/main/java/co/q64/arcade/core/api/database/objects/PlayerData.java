2780ce0d0772a8d8f760bda98a657266877ae9dd
Dylan <Quantum64Development@gmail.com>
2017-02-22 22:00:59 -0500

    Add player data api methods

--- CoreAPI/src/main/java/co/q64/arcade/core/api/database/objects/PlayerData.java
+++ CoreAPI/src/main/java/co/q64/arcade/core/api/database/objects/PlayerData.java
@@ -1,5 +1,11 @@
 package co.q64.arcade.core.api.database.objects;
 
 public interface PlayerData {
+	public String getName();
 
+	public void setName(String name);
+
+	public String getPlayer();
+
+	public void setPlayer(String player);
 }
