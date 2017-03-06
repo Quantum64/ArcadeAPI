961c8a7fb18eb7c0fbbd34ef3ff3f63895534c25
Dylan <Quantum64Development@gmail.com>
2017-03-06 17:17:57 -0500

    Add permission mananger docs

--- CoreAPI/src/main/java/co/q64/arcade/core/api/managers/PermissionManager.java
+++ CoreAPI/src/main/java/co/q64/arcade/core/api/managers/PermissionManager.java
@@ -15,20 +15,63 @@ import co.q64.arcade.core.api.perm.Group;
  */
 @Injectable
 public interface PermissionManager {
+	/**
+	 * Enables the permission manager
+	 * Automatically called on core init
+	 */
 	public void enable();
 
+	/**
+	 * Returns if a player is in the provided group or any extension of that group
+	 * @param cs the requester
+	 * @param g the group
+	 * @return if the player is in the group
+	 */
 	public boolean inGroup(CommandSender cs, Group g);
 
+	/**
+	 * Returns if a player is in the provided group exactly
+	 * @param cs the requester
+	 * @param g the group
+	 * @return if the player is in the group
+	 */
 	public boolean inGroupExact(CommandSender cs, Group g);
 
+	/**
+	 * Returns if a player is in the provided group or any extension of that group
+	 * @param g the group
+	 * @return if the player is in the group
+	 */
 	public boolean inGroup(UUID u, Group g);
 
+	/**
+	 * Returns if a player is in the provided group exactly
+	 * @param g the group
+	 * @return if the player is in the group
+	 */
 	public boolean inGroupExact(UUID u, Group g);
 
+	/**
+	 * Removes all groups for a player
+	 * @param sender the requester
+	 * @param playerName the player to remove groups from
+	 */
 	public void clear(CommandSender sender, String playerName);
 
+	/**
+	 * Removes a player from all groups except the provided one
+	 * @param sender the requester
+	 * @param playerName the player
+	 * @param group the group to set the player to
+	 */
 	public void setGroup(CommandSender sender, String playerName, String group);
 
+	/**
+	 * Adds a group to the player's group list
+	 * @param sender the requester
+	 * @param playerName
+	 * @param group
+	 */
 	public void addGroup(CommandSender sender, String playerName, String group);
 
 	public void removeGroup(CommandSender sender, String playerName, String group);
