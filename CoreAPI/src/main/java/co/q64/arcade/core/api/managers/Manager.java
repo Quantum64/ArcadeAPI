ab703b95f9c47dc38f6da73f1abdf8509c8674e5
Dylan <Quantum64Development@gmail.com>
2017-03-08 12:05:39 -0500

    Finish permission mamanger docs

--- CoreAPI/src/main/java/co/q64/arcade/core/api/managers/Manager.java
+++ CoreAPI/src/main/java/co/q64/arcade/core/api/managers/Manager.java
@@ -2,11 +2,14 @@ package co.q64.arcade.core.api.managers;
 
 import javax.inject.Inject;
 
+import co.q64.arcade.core.api.annotation.Noinject;
+
 /**
  * Represents a stateless subsystem of this component
  * @author Dylan
  *
  */
+@Noinject
 public interface Manager {
 	/**
 	 * An injected method that is used to complete the initialization of the manager after its construction.
--- CoreAPI/src/main/java/co/q64/arcade/core/api/managers/PermissionManager.java
+++ CoreAPI/src/main/java/co/q64/arcade/core/api/managers/PermissionManager.java
@@ -11,7 +11,6 @@ import co.q64.arcade.core.api.perm.Group;
 /**
  * The central component of AP (arcade permissions)
  * @author Dylan
- * DOCUMENTATION NEEDED!
  */
 @Injectable
 public interface PermissionManager {
@@ -23,33 +22,35 @@ public interface PermissionManager {
 
 	/**
 	 * Returns if a player is in the provided group or any extension of that group
-	 * @param cs the requester
-	 * @param g the group
+	 * @param sender the requester
+	 * @param group the group
 	 * @return if the player is in the group
 	 */
-	public boolean inGroup(CommandSender cs, Group g);
+	public boolean inGroup(CommandSender sender, Group group);
 
 	/**
 	 * Returns if a player is in the provided group exactly
-	 * @param cs the requester
-	 * @param g the group
+	 * @param senser the requester
+	 * @param group the group
 	 * @return if the player is in the group
 	 */
-	public boolean inGroupExact(CommandSender cs, Group g);
+	public boolean inGroupExact(CommandSender sender, Group group);
 
 	/**
 	 * Returns if a player is in the provided group or any extension of that group
-	 * @param g the group
+	 * @param uuid the player check the group of
+	 * @param group the group
 	 * @return if the player is in the group
 	 */
-	public boolean inGroup(UUID u, Group g);
+	public boolean inGroup(UUID uuid, Group group);
 
 	/**
 	 * Returns if a player is in the provided group exactly
+	 * @param uuid the player check the group of
 	 * @param g the group
 	 * @return if the player is in the group
 	 */
-	public boolean inGroupExact(UUID u, Group g);
+	public boolean inGroupExact(UUID uuid, Group group);
 
 	/**
 	 * Removes all groups for a player
@@ -69,21 +70,48 @@ public interface PermissionManager {
 	/**
 	 * Adds a group to the player's group list
 	 * @param sender the requester
-	 * @param playerName
-	 * @param group
+	 * @param playerName the name of the player to add thr group of
+	 * @param group the group to add them to
 	 */
 	public void addGroup(CommandSender sender, String playerName, String group);
 
+	/**
+	 * Removes a group from the player's group list
+	 * Sends the result to the requester
+	 * @param sender the requester
+	 * @param playerName the player to remove thr group from
+	 * @param group the group to remove
+	 */
 	public void removeGroup(CommandSender sender, String playerName, String group);
 
+	/**
+	 * Sends all the groups a player is in to the requester
+	 * @param sender the requester
+	 * @param playerName the name of the player to get the groups of
+	 */
 	public void listGroups(CommandSender sender, String playerName);
 
+	/**
+	 * Get all the classes that contain registered groups, this will almost alwyas be enums
+	 * @return the list of group providers
+	 */
 	public List<Class<? extends Group>> getGroupProviders();
 
-	public void registerGroupProvider(Class<? extends Group> clazz);
+	/**
+	 * Adds a new group provider
+	 * @param provider the enum of groups to add
+	 */
+	public void registerGroupProvider(Class<? extends Group> provider);
 
+	/**
+	 * Allows arcade to change the groups of players to arcade groups
+	 * for any permissions plugin installed
+	 */
 	public void enablePermissionsLink();
 
+	/**
+	 * Gets a list of all groups in increasing priority
+	 * @return the list of groups
+	 */
 	public List<Group> getGroups();
-
 }
