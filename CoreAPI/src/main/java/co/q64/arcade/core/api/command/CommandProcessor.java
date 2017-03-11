c185d1b8f15d9ad57c9a2e4b05ff7fdfc719981c
Dylan Ryman <Quantum64@users.noreply.github.com>
2017-03-10 21:27:19 -0500

    Update CommandProcessor.java

--- CoreAPI/src/main/java/co/q64/arcade/core/api/command/CommandProcessor.java
+++ CoreAPI/src/main/java/co/q64/arcade/core/api/command/CommandProcessor.java
@@ -16,7 +16,7 @@ import co.q64.arcade.core.api.perm.Group;
 public interface CommandProcessor {
 	/**
 	 * We do not currently provide a public command hander API for adding arcade style commands,
-	 * but you could make your own if you want, the register your commands in our multibinder
+	 * but you could make your own if you want, then register your commands in our multibinder
 	 * @param processor the command processor to add
 	 */
 	public void registerMod(CommandProcessor processor);
