8f867eb61f0c5798512b3a5753b528a6898706f7
Dylan <Quantum64Development@gmail.com>
2017-03-17 22:24:09 -0400

    Add nullable to reward factory groups

--- CoreAPI/src/main/java/co/q64/arcade/core/api/reward/RewardTemplateFactory.java
+++ CoreAPI/src/main/java/co/q64/arcade/core/api/reward/RewardTemplateFactory.java
@@ -2,11 +2,13 @@ package co.q64.arcade.core.api.reward;
 
 import java.util.List;
 
+import javax.annotation.Nullable;
+
 import co.q64.arcade.core.api.annotation.Injectable;
 import co.q64.arcade.core.api.implementable.PlayerExecutor;
 import co.q64.arcade.core.api.perm.Group;
 
-@Injectable	
+@Injectable
 public interface RewardTemplateFactory {
-	public RewardTemplate create(String name, List<String> rewards, long delay, int slot, boolean claimOnce, Group group, PlayerExecutor onClaim);
+	public RewardTemplate create(String name, List<String> rewards, long delay, int slot, boolean claimOnce, @Nullable Group group, PlayerExecutor onClaim);
 }
