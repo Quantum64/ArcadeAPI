9e82b4a269651228f880f7a4f0ca01e869a8b8e7
Dylan <Quantum64Development@gmail.com>
2017-03-22 11:54:09 -0400

    Add pair impl as lang

---
+++ CoreAPI/src/main/java/co/q64/arcade/core/api/lang/Pair.java
@@ -0,0 +1,22 @@
+package co.q64.arcade.core.api.lang;
+
+import co.q64.arcade.core.api.annotation.Noinject;
+
+@Noinject
+public class Pair<X, Y> {
+	private final X x;
+	private final Y y;
+
+	public Pair(X x, Y y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public X getX() {
+		return x;
+	}
+
+	public Y getY() {
+		return y;
+	}
+}
