638b5ee285c0163ad31c55747840c63799791ccf
Dylan <Quantum64Development@gmail.com>
2017-04-01 04:24:33 -0400

    Add new hologram API

--- CoreAPI/src/main/java/co/q64/arcade/core/api/managers/HologramManager.java
+++ CoreAPI/src/main/java/co/q64/arcade/core/api/managers/HologramManager.java
@@ -1,5 +1,9 @@
 package co.q64.arcade.core.api.managers;
 
+import java.util.UUID;
+
+import org.bukkit.Location;
+
 import co.q64.arcade.core.api.annotation.inject.Global;
 import co.q64.arcade.core.api.annotation.inject.Injectable;
 import co.q64.arcade.core.api.holo.HoloTextGetter;
@@ -7,5 +11,9 @@ import co.q64.arcade.core.api.holo.HoloTextGetter;
 @Injectable
 @Global
 public interface HologramManager extends Manager {
-	public void registerCustomHolo(String parentName, float yOffset, int triggerDistance, HoloTextGetter standName);
+	public UUID registerCustomHolo(String parentName, float yOffset, int triggerDistance, HoloTextGetter standName);
+
+	public UUID registerCustomHolo(Location location, float yOffset, int triggerDistance, HoloTextGetter standName);
+
+	public void destroyHolo(UUID id);
 }
