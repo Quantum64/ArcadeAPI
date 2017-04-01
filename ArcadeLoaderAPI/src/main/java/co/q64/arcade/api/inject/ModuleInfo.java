fe64371deb87de20c65e40503d3b3637afa8cb7f
Dylan <Quantum64Development@gmail.com>
2017-04-01 17:32:25 -0400

    Add module info docsa

--- ArcadeLoaderAPI/src/main/java/co/q64/arcade/api/inject/ModuleInfo.java
+++ ArcadeLoaderAPI/src/main/java/co/q64/arcade/api/inject/ModuleInfo.java
@@ -2,6 +2,15 @@ package co.q64.arcade.api.inject;
 
 import java.util.Map;
 
+/**
+ * Contains information about the modules in this universe
+ * @author Dylan
+ *
+ */
 public interface ModuleInfo {
+	/**
+	 * Gets a list of all loaded modules
+	 * @return the list of modules
+	 */
 	public Map<AbstractArcadeModule, String> getModules();
 }
