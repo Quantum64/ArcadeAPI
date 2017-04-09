3f1a38d357e06f6129f17d80dfb6365e02cbcaaa
Dylan <Quantum64Development@gmail.com>
2017-04-09 02:14:51 -0400

    Add new data controller methods

--- CoreAPI/src/main/java/co/q64/arcade/core/api/database/GeneralDataController.java
+++ CoreAPI/src/main/java/co/q64/arcade/core/api/database/GeneralDataController.java
@@ -1,7 +1,13 @@
 package co.q64.arcade.core.api.database;
 
+import co.q64.arcade.core.api.database.objects.GeneralData;
+
 public interface GeneralDataController {
 	public void update(String value);
 
 	public void update(String key, String value);
+
+	public void update(GeneralData data, String value);
+
+	public void update(GeneralData data, String key, String value);
 }
