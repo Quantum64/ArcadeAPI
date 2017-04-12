4883dd9418fddfcfe2295ca8df62f75e757ef211
Dylan <Quantum64Development@gmail.com>
2017-04-12 01:11:29 -0400

    Add crate message provider

---
+++ CoreAPI/src/main/java/co/q64/arcade/core/api/crate/CrateMessageProvider.java
@@ -0,0 +1,10 @@
+package co.q64.arcade.core.api.crate;
+
+import co.q64.arcade.core.api.annotation.inject.Global;
+import co.q64.arcade.core.api.annotation.inject.Injectable;
+
+@Injectable
+@Global
+public interface CrateMessageProvider {
+	public String getCrateFindMessage();
+}
