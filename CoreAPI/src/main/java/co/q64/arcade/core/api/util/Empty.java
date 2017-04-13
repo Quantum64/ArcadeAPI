7331baf7c6df68244d15e4d6391fb0f5b11a1b4b
Dylan <Quantum64Development@gmail.com>
2017-04-13 16:58:33 -0400

    Add empty interface

---
+++ CoreAPI/src/main/java/co/q64/arcade/core/api/util/Empty.java
@@ -0,0 +1,8 @@
+package co.q64.arcade.core.api.util;
+
+import co.q64.arcade.core.api.annotation.inject.Noinject;
+
+@Noinject
+public interface Empty {
+	public static final String STRING = new String();
+}
