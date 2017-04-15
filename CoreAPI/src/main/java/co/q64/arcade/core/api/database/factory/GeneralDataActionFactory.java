2bb812f71e5573ccf608317727df022311a55477
Dylan <Quantum64Development@gmail.com>
2017-04-15 02:10:28 -0400

    Make general data action factory methods generic

--- CoreAPI/src/main/java/co/q64/arcade/core/api/database/factory/GeneralDataActionFactory.java
+++ CoreAPI/src/main/java/co/q64/arcade/core/api/database/factory/GeneralDataActionFactory.java
@@ -11,16 +11,16 @@ import com.google.inject.assistedinject.Assisted;
 
 @Injectable
 @Global
-public interface GeneralDataActionFactory<T extends GeneralData> {
-	public GeneralDataAction<T> create(@Assisted("key") String key, @Assisted("value") String value, GeneralDataMultiScope<T> exec, Class<T> clazz);
+public interface GeneralDataActionFactory {
+	public <T extends GeneralData> GeneralDataAction<T> create(@Assisted("key") String key, @Assisted("value") String value, GeneralDataMultiScope<T> exec, Class<T> clazz);
 
-	public GeneralDataAction<T> create(String value, GeneralDataMultiScope<T> exec, Class<T> clazz);
-	
-	public GeneralDataAction<T> create(GeneralDataMultiScope<T> exec, Class<T> clazz);
+	public <T extends GeneralData> GeneralDataAction<T> create(String value, GeneralDataMultiScope<T> exec, Class<T> clazz);
 
-	public GeneralDataAction<T> create(@Assisted("key") String key, @Assisted("value") String value, GeneralDataSingleScope<T> exec, Class<T> clazz);
+	public <T extends GeneralData> GeneralDataAction<T> create(GeneralDataMultiScope<T> exec, Class<T> clazz);
 
-	public GeneralDataAction<T> create(String value, GeneralDataSingleScope<T> exec, Class<T> clazz);
-	
-	public GeneralDataAction<T> create(GeneralDataSingleScope<T> exec, Class<T> clazz);
+	public <T extends GeneralData> GeneralDataAction<T> create(@Assisted("key") String key, @Assisted("value") String value, GeneralDataSingleScope<T> exec, Class<T> clazz);
+
+	public <T extends GeneralData> GeneralDataAction<T> create(String value, GeneralDataSingleScope<T> exec, Class<T> clazz);
+
+	public <T extends GeneralData> GeneralDataAction<T> create(GeneralDataSingleScope<T> exec, Class<T> clazz);
 }
