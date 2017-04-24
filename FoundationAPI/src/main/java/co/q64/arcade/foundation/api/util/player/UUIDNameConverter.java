44d0fee095fcaae11c64b3a6ede1789761dbc7eb
Dylan <Quantum64Development@gmail.com>
2017-04-24 10:32:10 -0400

    Move annotations

--- FoundationAPI/src/main/java/co/q64/arcade/foundation/api/util/player/UUIDNameConverter.java
+++ FoundationAPI/src/main/java/co/q64/arcade/foundation/api/util/player/UUIDNameConverter.java
@@ -1,5 +1,8 @@
 package co.q64.arcade.foundation.api.util.player;
 
-public interface UUIDNameConverter {
+import java.util.Optional;
+import java.util.UUID;
 
+public interface UUIDNameConverter {
+	public Optional<String> uuidToName(UUID uuid);
 }
